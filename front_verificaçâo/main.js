b4ec8f707b8d43c9841148f4c459d1d77fb6b677
7jotape7 <jpc0848@gmail.com>
2022-09-01 12:15:08 -0300

    coloquei um form no html e fiz o codigo do js

--- front_verificaçâo/main.js
+++ front_verificaçâo/main.js
@@ -1,1 +1,17 @@
-main.js
+let email = document.getElementById ("email");
+
+form.addEventListener('submit', (e) =>{
+    if(email.value == "") {
+        textform.textcontent("Preencha com um e-mail valído." <br> "Ex.: abc@com");
+    }
+        else(
+            console.log(email.value);
+        )
+
+        e.preventDefault()
+})
+
+function validatorEmail(email){
+    let emailPattern = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$/;
+    return emailPattern.test(email);
+}
